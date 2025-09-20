87a5ced9089cb76de0a8381dcf4cc6da568cf133
grl <857308047@qq.com>
2025-09-20 15:28:42 +0800

    控制台输出字符（供printf使用）

--- src/devs/console.c
+++ src/devs/console.c
@@ -24,9 +24,9 @@ consputc(int c)
 {
   if(c == BACKSPACE){
     // if the user typed backspace, overwrite with a space.
-    // uartputc_sync('\b'); uartputc_sync(' '); uartputc_sync('\b');
+    uartputc_sync('\b'); uartputc_sync(' '); uartputc_sync('\b');
   } else {
-    // uartputc_sync(c);
+    uartputc_sync(c);
   }
 }
 

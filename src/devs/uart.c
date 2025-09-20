23f856f223192ae86cfc04daeff861d6c231b2b8
grl <857308047@qq.com>
2025-09-20 15:29:54 +0800

    字符级别的同步机制

--- src/devs/uart.c
+++ src/devs/uart.c
@@ -12,14 +12,30 @@ void uart_putc(uint8 c){
     *(volatile uint8*)(UART0 + THR) = c;
 }
 
+
+// TODO: 缓冲区机制解决外设速度不匹配
+void uartputc_sync(uint8 c){
+
+    acquire(&uart_tx_lock);
+
+    if(panicked){
+       for(;;) ;
+    }
+
+    while((*(volatile uint8 *)(UART0 + LSR) & TX_IDLE) == 0);
+    *(volatile uint8*)(UART0 + THR) = c;
+
+    release(&uart_tx_lock);
+}
+
 void uart_puts(char * s){
-    // accquire
+    // acquire --> 交给 printf 的功能
     while (*s != '\0')
     {
-        uart_putc(*s);
+        //  字符锁，保证不会掉字符，但不保证句间完整性
+        uartputc_sync(*s);
         s++;
     }
-    // release
 }
 
 void uartinit(){

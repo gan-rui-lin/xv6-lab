02dd23b9cdbea43f882b1c7be6e7d06d02f1e058
grl <857308047@qq.com>
2026-01-01 19:45:11 +0800

    f

--- user/initcode.c
+++ user/initcode.c
@@ -82,7 +82,7 @@ int main()
     test_("open");
     test_("read");
     test_("brk");
-    test_("mmap");
+
     test_("openat");
     test_("getpid");
     test_("exit");
@@ -99,10 +99,12 @@ int main()
     // test_("dup2");  //! failed
     test_("getdents"); //! wrong getdents fd:1
     // test_("mount"); //! exception
-    // test_("pipe");  //? maybe right
-    // // test_("fstat"); //! wrong
-    // test_("write");
-    // test_("uname");
+    test_("pipe");  //? maybe right
+    test_("fstat"); //! wrong
+    test_("write");
+    test_("uname");
+        test_("mmap");
+    test_("munmap");
 
     // test_busybox();
     shutdown();
